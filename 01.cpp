7b02badce9aa70dad7421db0fdb119259cde9781
jorden134677 <48340565+jorden134677@users.noreply.github.com>
2019-03-15 11:51:05 +0800

    Add files via upload

---
+++ 01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	printf("Hello world 3A713207");
+	return 0;
+}
